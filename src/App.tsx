39b3c9de3737f78fc1a79d633523ebdb4401d50e
femnixx <femnitality@gmail.com>
2025-08-09 20:52:56 +0700

    Tested tailwind CSS, worked

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
+        <p className='font-semibold text-2xl'>Hi there</p>
       </div>
     </>
   )
