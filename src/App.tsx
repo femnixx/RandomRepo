a725cd9041d8dfaa735e785ff561d6dc92d95eb4
femnixx <femnitality@gmail.com>
2025-08-09 22:06:56 +0700

    Finished with setting up supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
+import './App.css';
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
 
-import './App.css'
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
 
-function App() {
+type User = {
+  id: number;
+  name: string;
+};
+
+function App() { 
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+  
+  async function getUsers() {
+    const { data, error } = await supabase.from("users").select();
+
+    if (error) {
+      console.error(error);
+    } else if (data) {
+      setUsers(data);
+    }
+  }
 
   return (
-    <>
-      <div>
-        <p className='font-semibold text-2xl'>Hi there</p>
-      </div>
-    </>
-  )
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {users.map((u) => (
+          <li key={u.id}>{u.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
