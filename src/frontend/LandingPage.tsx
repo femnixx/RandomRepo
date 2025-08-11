302e14a11f962b40a53f3e82a7fdd068f08c168a
femnixx <femnitality@gmail.com>
2025-08-11 09:54:52 +0700

    Added landing page

---
+++ src/frontend/LandingPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>
+        <div>
+            <p>Landing page</p>
+        </div>
+    </div>
+  )
+}
+
+export default LandingPage
--- src/frontend/SignIn.tsx
+++ src/frontend/SignIn.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { supabase } from '../SupabaseClient'
+import { useState } from 'react'
 
 const SignIn = () => {
-  return (
-    <div>SignIn</div>
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+     
+    const handleLogin = async () => {
+        let { data error } = await supabase.auth.signInWithPassword
+    }
+  
+    return (
+    <div>
+        <div>
+            <p>Welcome back</p>
+            
+            <div className='mt-5'>
+                <input type="email" className='border-1 px-2'/>            
+            </div>
+        </div>
+    </div>
   )
 }
 
