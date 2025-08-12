4e12ccf40b01b675fd8a7b7bcf75b1c990393dd7
femnixx <femnitality@gmail.com>
2025-08-12 22:07:27 +0700

    Added submit button and put padding and margin on the elements

--- src/frontend/SignUp.tsx
+++ src/frontend/SignUp.tsx
@@ -28,24 +28,25 @@ const SignUp = () => {
   }
   return (
     <>
-    <form>
-      <div>
+    <form onSubmit={handleSignUp}>
+      <div className=''>
         <h1>Create An Account</h1>
-        <div className="flex flex-col gap-y-3 mt-5">
+        <div className="flex flex-col gap-y-3 mt-5 ms-5">
           <p>Username</p>
           <input type="text" className="border-1 w-fit" value={username} onChange={(e) => setUsername(e.target.value)}/>
         </div>
-        <div className="flex flex-col gap-y-3">
+        <div className="flex flex-col gap-y-3 ms-5">
           <p>Email</p>
           <input type="email" className="border-1 w-fit" value={email} onChange={(e) => setEmail(e.target.value)}/>
         </div>
-        <div className="flex flex-col gap-y-3">
+        <div className="flex flex-col gap-y-3 ms-5">
           <p>Password</p>
           <input type="password" className="border-1 w-fit" value={password} onChange={(e) => setPassword(e.target.value)}/>
         </div>
-        <Link to="/">LandingPage</Link>
+        <button className='border-1 px-2 mt-5 ms-5' onClick={handleSignUp}>Submit</button>
       </div>
     </form>
+        <Link to="/">LandingPage</Link>
     </>
   )
 }
