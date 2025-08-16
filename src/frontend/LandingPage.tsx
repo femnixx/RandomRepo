8cb11cd184fde14300b1cec32a854d07eb521a62
femnixx <femnitality@gmail.com>
2025-08-16 16:18:07 +0700

    Error but commiting anyways

--- src/frontend/LandingPage.tsx
+++ src/frontend/LandingPage.tsx
@@ -1,13 +1,26 @@
 import '../App.css';
 import { Link } from 'react-router-dom';
-function App() { 
+import { supabase } from '../SupabaseClient';
+import { useEffect } from 'react';
+
+function App() {
+
+  useEffect(() => {
+    const getSession = async () => {
+      const { data, error } = await supabase.auth.getSession();
+      if (error) {
+        
+      }
+    }
+  })
+  
   return (
     <>
       <div>
         <p>Landing Page</p>
         <div className='flex flex-col mt-5 gap-y-3'>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Signup</Link>
+            <Link to="/login" className='w-fit'>Login</Link>
+            <Link to="/signup" className='w-fit'>Signup</Link>
         </div>
       </div>
     </>
