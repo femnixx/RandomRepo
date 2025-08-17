f6335376d05d70696ebd56044d19b89587304b5b
femnixx <femnitality@gmail.com>
2025-08-17 23:17:58 +0700

    Made username useState and navigate

--- src/frontend/components/LoggedIn.tsx
+++ src/frontend/components/LoggedIn.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import { supabase } from '../../SupabaseClient'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 const LoggedIn = () => {
+    const [username, setUsername] = useState('');
+    const navigate = useNavigate();
+
+    const handleSession = async () => {
+        const { data, error } = await supabase.auth.getSession();
+        if (data.session) {
+            const user = data.session.user;
+
+        }
+    }
   return (
     <div>
         <p>Hi there</p>
