dcc6ae2927c4fe86edb78db6780fee674d367410
femnixx <femnitality@gmail.com>
2025-08-17 15:01:01 +0700

    Made it so that supabase returns a name using user_metadata?.username

--- src/frontend/LandingPage.tsx
+++ src/frontend/LandingPage.tsx
@@ -1,27 +1,33 @@
 import '../App.css';
 import { Link } from 'react-router-dom';
 import { supabase } from '../SupabaseClient';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import type { User } from '@supabase/supabase-js';
 
 function App() {
+  const [name, setName] = useState<string>("");
 
   useEffect(() => {
-    const retrieveSession = async () => {
+    const sessionCheck = async () => {
       const { data, error } = await supabase.auth.getSession();
-      if (error) {
-        console.log(error)
-      } 
+      if (data.session) {
+        const user = data.session.user;
+        setName(user?.user_metadata?.username ?? "Guest");
+      }
     };
-    retrieveSession();
+
+    sessionCheck();
   }, []);
-  
+
   return (
     <>
       <div>
         <p>Landing Page</p>
+        <p>Current logged in as: {name || "No one"}</p> {/* ✅ render state here */}
+        
         <div className='flex flex-col mt-5 gap-y-3'>
-            <Link to="/login" className='w-fit'>Login</Link>
-            <Link to="/signup" className='w-fit'>Signup</Link>
+          <Link to="/login" className='w-fit'>Login</Link>
+          <Link to="/signup" className='w-fit'>Signup</Link>
         </div>
       </div>
     </>
