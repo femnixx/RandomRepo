cf5b13fde83ed56939d93940ba66a30ccc528796
femnixx <femnitality@gmail.com>
2025-08-18 09:41:47 +0700

    Added routes for rescuedanimal and foundanimal pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SignIn from './frontend/SignIn'
 import SignUp from './frontend/SignUp'
 import LoggedIn from './frontend/components/LoggedIn'
+import FoundAnimal from './frontend/FoundAnimal'
+import RescueAnimal from './frontend/RescueAnimal'
+
 const App = () => {
   return (
    <>
@@ -14,6 +17,8 @@ const App = () => {
           <Route path='/login' element={<SignIn />}/>
           <Route path='/signup' element={<SignUp />}/> 
           <Route path='/loggedin' element={<LoggedIn />}/> 
+          <Route path='/rescued' element={<RescueAnimal />}/>
+          <Route path='/found' element={<FoundAnimal />}/>
         </Routes>
       </BrowserRouter>
    </div>
