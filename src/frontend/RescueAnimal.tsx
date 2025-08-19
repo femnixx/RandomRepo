f0746128a7fc86b180fb8ebc9dbd2e4341659c6c
femnixx <femnitality@gmail.com>
2025-08-19 20:29:28 +0700

    Added a test and it didnt work, trying another

--- src/frontend/RescueAnimal.tsx
+++ src/frontend/RescueAnimal.tsx
@@ -3,14 +3,14 @@ import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 
 const RescueAnimal = () => {
-  const [image, setImage] = useState('')
-  
+  const [image, setImage] = useState<any[]>([]) // store image files
+  const [error, setError] = useState<string | null>(null)
   useEffect( () => {
     const DisplayImage = async () => {
       const { data, error } = await supabase
       .storage
-      .from('images')
-      .list('', {
+      .from('animals')
+      .list('cities', {
         limit: 2,
         offset: 0,
         sortBy: { column: 'name', order:'asc'}
@@ -19,7 +19,7 @@ const RescueAnimal = () => {
       if (error) {
         console.error(error)
       } else {
-        // do it here
+        setImage(data)
       }
     }
     DisplayImage()
@@ -29,7 +29,12 @@ const RescueAnimal = () => {
     <div>
       <p>RescueAnimal</p>
       <p>List Images:</p>
-      {DisplayImage}
+      {error && <p style={{ color: "red" }}>Error: {error}</p>}
+      <ul>
+        {image.map((img) => (
+          <li key={img.name}>{img.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
