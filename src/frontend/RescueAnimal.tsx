5691f97be99058219a8bb980dec6c824bdeecc2d
femnixx <femnitality@gmail.com>
2025-08-19 20:13:29 +0700

    Started with supabase storage handler

--- src/frontend/RescueAnimal.tsx
+++ src/frontend/RescueAnimal.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { supabase } from "../SupabaseClient"
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 const RescueAnimal = () => {
+
+  useEffect( () => {
+    const DisplayImage = async () => {
+      const { data, error } = await supabase
+      .storage
+      .from('images')
+      .list('', {
+        limit: 2,
+        offset: 0,
+        sortBy: { column: 'name', order:'asc'}
+      })
+
+      if (error) {
+        console.error(error)
+      } else {
+        // do it here
+      }
+    }
+  })
+
   return (
-    <div>RescueAnimal</div>
+    <div>
+      <p>RescueAnimal</p>
+      <p>List Images:</p>
+      {DisplayImage}
+    </div>
   )
 }
 
