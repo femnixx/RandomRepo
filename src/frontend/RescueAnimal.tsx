b9c07bd3b671b3ee7770a33cd5708ef994adacb6
femnixx <femnitality@gmail.com>
2025-08-21 05:22:46 +0700

    Added listFiles function to list the files in a specific folder and subfolder in supabase storage

--- src/frontend/RescueAnimal.tsx
+++ src/frontend/RescueAnimal.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react"
 import { supabase } from "../SupabaseClient"
-
+import { useEffect } from "react";
 const RescueAnimal = () => {
+  type ImageFile = {
+    name: string;
+    url: string;
+  };
+  const [images, setImages] = useState<ImageFile[]>([]);
+  
   const listFiles = async () => {
     const { data, error } = await supabase
     .storage
@@ -9,8 +16,31 @@ const RescueAnimal = () => {
       limit:100,
       offset:0,
       sortBy: { column: 'name', order: 'asc'},
-    })
-  }
+    });
+
+    if (error) {
+      console.error('Error listing files: ', error.message);
+      return;
+    } 
+    if (data) {
+      // convert file names into public URLs
+      const urls = data.map((file) => {
+        const { data: publicUrl } = supabase.storage
+          .from("animals")
+          .getPublicUrl(`cities/${file.name}`);
+        return {
+          name: file.name,
+          url: publicUrl.publicUrl,
+        };
+      });
+      setImages(urls);
+    }
+  };
+  // Run listFiles once when componen mounts
+  useEffect(() => {
+    listFiles();
+  }, []);
+
   return (
     <div>
       <p>RescueAnimal</p>
