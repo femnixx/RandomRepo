968cd417a94664ac83fb05ade7a979aac4e6c949
femnixx <femnitality@gmail.com>
2025-08-21 13:40:25 +0700

    Deleted list images to found an animal

--- src/frontend/RescueAnimal.tsx
+++ src/frontend/RescueAnimal.tsx
@@ -2,58 +2,12 @@ import { useState } from "react"
 import { supabase } from "../SupabaseClient"
 import { useEffect } from "react";
 const RescueAnimal = () => {
-  type ImageFile = {
-    name: string;
-    url: string;
-  };
-  const [images, setImages] = useState<ImageFile[]>([]);
  
-  
-  const listFiles = async () => {
-    const { data, error } = await supabase
-    .storage
-    .from('animals')
-    .list('cities', {
-      limit:100,
-      offset:0,
-      sortBy: { column: 'name', order: 'asc'},
-    });
-
-    if (error) {
-      console.error('Error listing files: ', error.message);
-      return;
-    }   
-    if (data) {
-      // convert file names into public URLs
-      const urls = data.map((file) => {
-        const { data: publicUrl } = supabase.storage
-          .from("animals")
-          .getPublicUrl(`cities/${file.name}`);
-        return {
-          name: file.name,
-          url: publicUrl.publicUrl,
-        };
-      });
-      setImages(urls);
-    }
-  };
-  // Run listFiles once when componen mounts
-  useEffect(() => {
-    listFiles();
-  }, []);
-
   return (
     <div>
       <p>RescueAnimal</p>
       <p>List Images:</p>
-      <div className="grid grid-cols-3 gap-4">
-        {images.map((img) => (
-          <div key={img.name} className="border p-2">
-            <p>{img.name}</p>
-            <img src={img.url} alt={img.name} className="w-40 h-40 object-cover"/>
-          </div>
-        ))}
-      </div>
+      
     </div>
   )
 }
