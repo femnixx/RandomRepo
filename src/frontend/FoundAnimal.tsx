ce584ad91e3c3e6d83eb74f621eb16cc515d59b2
femnixx <femnitality@gmail.com>
2025-08-21 14:08:05 +0700

    Added onFileChange and upload function for testing purposes to see if it works

--- src/frontend/FoundAnimal.tsx
+++ src/frontend/FoundAnimal.tsx
@@ -1,6 +1,41 @@
 import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
 
 const FoundAnimal = () => {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  
+  const onFilechange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+  const onFileUpload = () => {
+    const formData = new FormData();
+    if (!selectedFile) {
+      console.error("No file selected");
+      return;
+    }
+    formData.append(
+      "myFile",
+      selectedFile,
+      selectedFile.name
+    );
+    console.log(selectedFile);
+    axios.post("api/uploadfile",formData);
+  };
+  const fileData = () => {
+    if (selectedFile) {
+      return (
+        <p>File successfully uploaded.</p>
+      ) else {
+        return (
+          <div>
+            Something went wrong.
+          </div>
+        )
+      }
+    }
+  }
+
   return (
     <div>FoundAnimal</div>
   )
