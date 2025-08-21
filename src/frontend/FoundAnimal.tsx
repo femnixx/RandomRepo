470992629504814f035c565e3e1e303654d92ce4
femnixx <femnitality@gmail.com>
2025-08-22 06:54:13 +0700

    Added functions for current user and using useNavigate to navigate back to login page

--- src/frontend/FoundAnimal.tsx
+++ src/frontend/FoundAnimal.tsx
@@ -1,31 +1,41 @@
 import React from 'react'
 import axios from 'axios'
 import { useState } from 'react'
+import { supabase } from '../SupabaseClient'
+import { User } from '@supabase/supabase-js'
+import { useNavigate } from 'react-router-dom'
 
 const FoundAnimal = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
-  
-  const onFilechange = (event) => {
-    setSelectedFile(event.target.files[0]);
+  const navigate = useNavigate();     
+  const onFilechange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if ( event.target.files && event.target.files[0]) {
+      setSelectedFile(event.target.files[0]);
+    }
   };
-  const onFileUpload = () => {
-    const formData = new FormData();
-    if (!selectedFile) {
+
+  const onFileUpload = async () => {
+    
+    if (!selectedFile ) {
       console.error("No file selected");
       return;
+    } 
+    // get current user
+    const { data: { user } } = await supabase.auth.getUser();
+    if (!user) {
+      console.error("No user session found, please log in.")
+      navigate('/');
     }
-    formData.append(
-      "myFile",
-      selectedFile,
-      selectedFile.name
-    );
-    console.log(selectedFile);
-    axios.post("api/uploadfile",formData);
+    // use the selected file directly
+    const { data, error } = await supabase.storage
+      .from("images")
+      .upload(`users/${user.id}/${selectedFile.name}`, selectedFile)
   };
   const fileData = () => {
     if (selectedFile) {
       return (
-        <p>File successfully uploaded.</p>
+        window.alert('Successfully updated')
+        
       ) 
     }else {
         return (
