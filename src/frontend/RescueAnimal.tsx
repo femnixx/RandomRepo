e9961f7851f6d40f9abb9831fdde7e32770cbe35
femnixx <femnitality@gmail.com>
2025-08-22 21:05:00 +0700

    Made image retrieval work

--- src/frontend/RescueAnimal.tsx
+++ src/frontend/RescueAnimal.tsx
@@ -1,13 +1,14 @@
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../SupabaseClient";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const RescueAnimal = () => {
   const navigate = useNavigate();
-  
+  const [images, setImages] = useState<string[]>([]);
+
   const handleFileRetrieval = async () => {
     const { data: userData, error: sessionError } = await supabase.auth.getSession()
-     if (data.session) {
+     if (userData.session) {
       const userID = userData.session?.user.id;
       
       const { data: listUserItems, error: listError } = await supabase
@@ -17,22 +18,53 @@ const RescueAnimal = () => {
           limit:100,
           offset:0,
           sortBy: { column: "name", order: "asc"},
-        })
+        });
+        if (listError) {
+          console.error("Error listing files:", listError);
+          return;
+        }
 
+        // convert files metadata => signeDUrls
+        const signedUrls = await Promise.all(
+          listUserItems.map(async (file) => {
+            const { data, error } = await supabase.storage
+              .from('users')
+              .createSignedUrl(`${userID}/${file.name}`, 3600) // in seconds
+            if (error) {
+              console.error("Error creating signed URL:", error);
+              return null;
+            }
+            return data?.signedUrl || null;
+            })
+        );
+
+        // 3. save to state (remove nulls)
+        setImages(signedUrls.filter(Boolean) as string[]);
      } else {
       console.error(sessionError)
+      return;
      }
-  } 
+  } ;
+
+  useEffect(() => {
+    handleFileRetrieval();
+  }, [])
 
   const handleNavigation = () => {
     navigate('/')
   }
  
-  return (
+  return(
     <div>
       <p>RescueAnimal</p>
       <p>List Images:</p>
-      
+
+      {/* fetch images */}
+      <div>
+        {images.map((url, idx) => {
+          return  <img key={idx} src={url} alt="rescue" />
+        })}
+      </div>
       <button onClick={handleNavigation} className="flex mt-5 px-2 py-2 border-1">Return to landing page</button>
     </div>
   )
