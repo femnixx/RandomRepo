f717b7573281110e496ad80a032dd8c533ef8540
femnixx <femnitality@gmail.com>
2025-08-22 19:15:49 +0700

    Added return to landing page to rescuedanimals.tsx

--- src/frontend/RescueAnimal.tsx
+++ src/frontend/RescueAnimal.tsx
@@ -1,13 +1,21 @@
 import { useState } from "react"
 import { supabase } from "../SupabaseClient"
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 const RescueAnimal = () => {
+  const navigate = useNavigate();
+
+  const handleNavigation = () => {
+    navigate('/')
+  }
  
   return (
     <div>
       <p>RescueAnimal</p>
       <p>List Images:</p>
       
+      <button onClick={handleNavigation} className="flex mt-5 px-2 py-2 border-1">Return to landing page</button>
     </div>
   )
 }
