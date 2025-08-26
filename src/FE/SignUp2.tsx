2b6dacb5cd21665a06c3f1cbe3aa699f588ff105
femnixx <femnitality@gmail.com>
2025-08-26 10:09:30 +0700

    Started building form fields

--- src/FE/SignUp2.tsx
+++ src/FE/SignUp2.tsx
@@ -4,13 +4,18 @@ const SignUp2 = () => {
   return (
     <div className='bg-[#FF9748] w-screen h-screen'>
           <div className='pt-20'>
-            <div className='rounded-xs flex text-center flex-col bg-white h-fit ml-5 mr-5'>
+            <div className='rounded-sm flex text-center flex-col bg-white h-fit ml-5 mr-5'>
               <p>Register With</p>
               <div className='mt-1 flex justify-center text-center'>
                 <div className='w-fit flex gap-x-3 bg-[#C6ED73] px-5 rounded-full'>
                   <p>F</p>
                   <p>G</p>
                 </div>
+              </div>
+              <p>Or fill out these fields below</p>
+              {/* For  fields */}
+              <div>
+                
               </div>
             </div>
           </div>
