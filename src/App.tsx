d0756399b8083edf8dd40d41fc94537bd6ab92fe
femnixx <femnitality@gmail.com>
2025-08-26 08:56:44 +0700

    Added some routes for frontend

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,17 @@ import SignUp from './frontend/SignUp'
 import LoggedIn from './frontend/components/LoggedIn'
 import FoundAnimal from './frontend/FoundAnimal'
 import RescueAnimal from './frontend/RescueAnimal'
-
+import SignUp2 from './FE/SignUp2'
+import LoggedIn2 from './FE/LoggedIn2'
 const App = () => {
   return (
    <>
    <div>
    <BrowserRouter>
         <Routes>
+          <Route path='/signup' element={<SignUp2 />}/> 
+          <Route path='/login' element={<LoggedIn2 />}/> 
+          {/* Backend routes */}
           <Route path='/BE' element={<LandingPage />}/>
           <Route path='/BElogin' element={<SignIn />}/>
           <Route path='/BEsignup' element={<SignUp />}/> 
